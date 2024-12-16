7304bed218c3e9459d9e61279c57ce1e11f1b5ca
LiamHayden <20211287@dfei.ie>
2024-12-16 08:37:04 +0000

    Updaed data.sql and added error handling.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,18 @@
 CREATE TABLE IF NOT EXISTS customer (
     customer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    accessUrl BIGINT
+    accessUrl VARCHAR(255)
     );
 
-INSERT INTO customer (name, accessUrl) VALUES ('OPW', 91934);
+    CREATE TABLE IF NOT EXISTS PRINTER (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    customer_id BIGINT,
+    model VARCHAR(255),
+    assetTag BIGINT,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+    );
+
+
+INSERT INTO customer (name, accessUrl) VALUES ('OPW', 'www.google.com');
+
+INSERT INTO printer (customer_id, model, assetTag) VALUES (1, 'E87640', 91964);
