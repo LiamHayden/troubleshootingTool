1c3e2fe96330d4200dad30d42b13bd521aa142ac
LiamHayden <20211287@dfei.ie>
2025-01-06 16:47:12 +0000

    removed unused import and added entity annotation

--- src/main/java/com/datapac/troubleshootingTool/Question/Question.java
+++ src/main/java/com/datapac/troubleshootingTool/Question/Question.java
@@ -1,9 +1,11 @@
 package com.datapac.troubleshootingTool.Question;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Question {
 
     @Id
